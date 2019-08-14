d3e5fabadbac159c6e08323035a50e3facd10c34
ANIL MN <anilmundaya@gmail.com>
2019-08-14 09:00:58 +0530

    main page done

---
+++ script.js
@@ -0,0 +1,8 @@
+function one(x){
+    return document.getElementById(x); }
+
+function demo(){
+    var vnumber= document.getElementById('vnumber')
+    console.log(vnumber)
+    //window.location.href='book.php'
+}
